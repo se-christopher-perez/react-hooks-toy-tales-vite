970ad3054946cc4ef93eb5c5fad002ecd9e47c52
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-05-13 14:11:24 -0400

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+
+  const [toys, setToys] = useState([])
   const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+
+    fetch("http://localhost:3001/toys")
+    .then(r => {
+
+      if(!r.ok) {throw new Error("ERROR: No fetch!")}
+
+      return r.json()
+
+    })
+    .then(data => {
+
+      console.log(data)
+      setToys(data)
+
+    })
+    .catch(err => { console.log(err) })
+
+  }, [])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +40,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} />
     </>
   );
 }
